6d27789a058dc9397ed4dd7da5a7f1076c9a3d69
Lilu14Vasara <vasaralilu@gmail.com>
2025-07-14 20:27:41 +0530

    initial message

--- server.js
+++ server.js
@@ -82,4 +82,6 @@ app.use("/api", contactRoutes);
 // MongoDB Connection
 connectDB();
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
